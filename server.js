4a8c246b1da45c415d5f5f1d3eeaf5b4d2124277
Ansh Jaiswal <anshjaiswal1804@gmail.com>
2024-09-14 17:52:42 +0530

    new-changes

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 const port = 8000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
-});
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-});
+})
 app.get('/about', (req, res) => {
   res.send('<h1>About page</h1>');
-});
+})
