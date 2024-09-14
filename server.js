672fd84cbc0771445aa673c59f4d1578ad45f426
Ansh Jaiswal <anshjaiswal1804@gmail.com>
2024-09-14 17:42:24 +0530

    first-commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+app.get('/about', (req, res) => {
+  res.send('<h1>About page</h1>');
+});
